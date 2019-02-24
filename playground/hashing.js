a783ce774706d752f6c41919099fbf1742954c7f
Leon Cheung <LWC@Leons-MacBook-Pro.local>
2019-02-23 22:02:27 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+// Async call, 2 args - # of rounds
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$c77hYs8dCtgeBIaiYZvoiuVJzw1mw3EIfzIHiFUrxq7mL6BbjNOZW';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});		
 
 /************* JWT *************/
-var data = {
-  id: 10
-};
+// var data = {
+//   id: 10
+// };
 
 /* JWT: Header | Payload | Verify Signature */
 
-var token = jwt.sign(data, 'secretabc');
-console.log(token);
+// var token = jwt.sign(data, 'secretabc');
+// console.log(token);
 
-var decoded = jwt.verify(token, 'secretabc');
-console.log('decoded:', decoded);
+// var decoded = jwt.verify(token, 'secretabc');
+// console.log('decoded:', decoded);
 
 /************* crypto-js *************/
 // var message = 'I am user number 3';
